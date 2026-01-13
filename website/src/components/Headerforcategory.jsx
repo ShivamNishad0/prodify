052037993d8f8bdf9d627fefe7776221a9477d62
shivamnishad0 <2k23.mca2313672@gmail.com>
2026-01-13 23:03:55 +0530

    new component added for the choosing the category

--- website/src/components/Headerforcategory.jsx
+++ website/src/components/Headerforcategory.jsx
@@ -1,6 +1,11 @@
 function Headerforcategory(){
     return(
         <div className="header-category">
+        <div className="header-category-content">
+          <h1>Explore Our Exclusive Categories</h1>
+          <p>Discover a wide range of products tailored to your needs.</p>
+          <button>Explore Categories</button>
+        </div>
             
         </div>
     )
